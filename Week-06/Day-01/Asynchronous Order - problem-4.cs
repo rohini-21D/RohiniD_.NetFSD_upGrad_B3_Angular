c3c211b1b17975ab8b0ae1587806358e06678013
rohini-21D <dabbararohini343@gmail.com>
2026-03-23 18:25:04 +0530

    Add files via upload

---
+++ Week-06/Day-01/Asynchronous Order - problem-4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+       
+        static async Task VerifyPaymentAsync()
+        {
+            Console.WriteLine("Verifying Payment");
+            await Task.Delay(2000);
+            Console.WriteLine("Payment successsfull ");
+        }
+
+        static async Task CheckInventoryAsync()
+        {
+            Console.WriteLine("Checking Inventory");
+            await Task.Delay(3000);
+            Console.WriteLine("Available Inventory");
+        }
+
+        static async Task ConfirmOrderAsync()
+        {
+            Console.WriteLine("Confirming Order");
+            await Task.Delay(4000);
+            Console.WriteLine("Order Confirmed...");
+        }
+        static async Task Main()
+        {
+            Console.WriteLine("Order Processing Started");
+            //Execute steps asynchronously while maintaining the logical order of operations.
+           
+            await VerifyPaymentAsync();
+            await CheckInventoryAsync();
+            await ConfirmOrderAsync();
+
+            Console.WriteLine("\n Order Process Completed Succesfully");
+            Console.ReadLine();
+        }
+    }
+
+}
