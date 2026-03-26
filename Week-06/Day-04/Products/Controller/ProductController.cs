d5464ec0308efac3ddc483962c29c6d33177f9b6
rohini-21D <dabbararohini343@gmail.com>
2026-03-26 15:24:07 +0530

    Add files via upload

---
+++ Week-06/Day-04/Products/Controller/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>()
+            {
+                new Product { ProdId = 1, ProdName = "Laptop", ProdCategory="Electronics" ,ProdPrice = 50000 },
+                new Product { ProdId = 2, ProdName = "Mobile", ProdCategory="Electronics", ProdPrice = 20000 },
+                new Product { ProdId = 3, ProdName = "Tablet",ProdCategory="Electronics", ProdPrice = 15000 }
+            };
+        
+        //Index 	---	to display collection of products
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        //b.Details ---  to display  single product information
+        public IActionResult Details(int id)
+        {
+            var product = products.Find(p => p.ProdId == id);
+            return  View(product);
+        }
+    }
+}
