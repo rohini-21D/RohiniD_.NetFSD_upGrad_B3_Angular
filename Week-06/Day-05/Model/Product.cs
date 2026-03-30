4b155afa1ed0572c713ced85bb3b8f6886e15dde
rohini-21D <dabbararohini343@gmail.com>
2026-03-30 10:39:52 +0530

    Add files via upload

---
+++ Week-06/Day-05/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ProdId { get; set; }
+        [Required]
+        [Length(5,15)]
+        public string? ProdName { get; set; }
+        [Required]
+        [Length(5,15)]
+        public string? ProdCategory {  get; set; }
+        [Required]
+        [Range(1,1000000)]
+        public decimal ProdPrice { get; set; }
+
+    }
+}
