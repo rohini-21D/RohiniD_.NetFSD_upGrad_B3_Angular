67c5d0af7045264e67aab1297f89392ae09f9d60
rohini-21D <dabbararohini343@gmail.com>
2026-04-01 09:13:16 +0530

    Add files via upload

---
+++ Week-07/Day-02/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price {  get; set; }
+        public int Quantity {  get; set; }
+
+    }
+}
