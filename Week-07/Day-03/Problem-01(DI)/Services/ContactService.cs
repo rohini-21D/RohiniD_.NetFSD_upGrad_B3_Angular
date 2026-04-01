04cc5da944684ee7b4e4ef953a3a37d35099da16
rohini-21D <dabbararohini343@gmail.com>
2026-04-01 19:11:00 +0530

    Add files via upload

---
+++ Week-07/Day-03/Problem-01(DI)/Services/ContactService.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication4.Models;
+namespace WebApplication4.Services
+{
+    public class ContactService : IContactService
+    {
+        private static List<ContactInfo> contacts=new List<ContactInfo>();
+        
+        public void AddContact(ContactInfo contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public List<ContactInfo> GetAllContacts()
+        {
+            return contacts;
+        }
+
+        public ContactInfo GetContactById(int id)
+        {
+            return contacts.FirstOrDefault(c => c.ContactId == id);
+        }
+    }
+}
