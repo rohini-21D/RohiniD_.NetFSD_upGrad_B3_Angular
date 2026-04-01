c898822992a7bfadffb672f2f3d977ab5e7aa5d3
rohini-21D <dabbararohini343@gmail.com>
2026-04-01 19:15:46 +0530

    Add files via upload

---
+++ Week-07/Day-03/Problem-02(EF)/Models/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //Creatings atbles for Db
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
---
+++ Week-07/Day-03/Problem-02(EF)/Models/Movies.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Models
+{
+    public class Movies
+    {
+        public int Id {  get; set; }
+        [Required(ErrorMessage ="Title is mandatory"),StringLength(20,MinimumLength =4)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Genre is mandatory"), StringLength(20, MinimumLength = 4)]
+        public string Genre {  get; set; }
+
+        [Required(ErrorMessage = "ReleaseDate is mandatory"),DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Price is mandatory"),Column(TypeName ="decimal(10,2)")]
+        public decimal Price {  get; set; }
+        public string Rating {  get; set; }
+    }
+}
