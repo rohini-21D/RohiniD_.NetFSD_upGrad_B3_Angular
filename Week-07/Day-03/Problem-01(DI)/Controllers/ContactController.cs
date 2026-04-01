cbc4738f3e1d8b1a32502612d0fd3ed6cf6a887f
rohini-21D <dabbararohini343@gmail.com>
2026-04-01 19:09:24 +0530

    Add files via upload

---
+++ Week-07/Day-03/Problem-01(DI)/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+using WebApplication4.Services;
+
+namespace WebApplication4.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+        //all contactts
+        public IActionResult ShowAllContacts()
+        {
+            var contacts = _contactService.GetAllContacts();
+            return View(contacts);
+        }
+
+        //Get by id
+        public IActionResult GetContactById(int id)
+        {
+            var contact=_contactService.GetContactById(id);
+            return View(contact);
+        }
+
+        //get Add
+        [HttpGet]
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _contactService.AddContact(contact);
+                return RedirectToAction("ShowAllContacts");
+            }
+            return View(contact);
+        }
+    }
+}
